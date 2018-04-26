9182b8fa89d375cf8ec5cfa503dd9d18da75dc61
noob_Jedi <Boveri.asea@gmail.com>
2018-04-26 20:02:47 +0200

    - Added Main Scene - Added Story Scene

---
+++ project_beta/Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    public Image imageBackground;
+    public string imageBackgroundString = "";
+    Texture2D img;
+    
+
+	// Use this for initialization
+	void Start () {
+        Console.WriteLine("Test if ImageLoaded");
+        imageBackground.sprite = (Sprite)Resources.Load<Sprite>("Background/"+imageBackgroundString) as Sprite;
+        Console.WriteLine("Test if ImageLoaded 2");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Console.WriteLine("Test if ImageLoaded");
+    }
+}
